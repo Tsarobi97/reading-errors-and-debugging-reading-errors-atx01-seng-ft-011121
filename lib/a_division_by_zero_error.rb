5ff7bb17defd00d4b75db98566f8a5b3b6387c32
Robert Obi <robobi97@googlemail.com>
2020-12-24 03:58:16 +0000

    Automatically backed up by Learn

--- lib/a_division_by_zero_error.rb
+++ lib/a_division_by_zero_error.rb
@@ -2,3 +2,9 @@
 
 42 / 0
 
+context 'ZeroDivisionError' do
+    it 'raises a ZeroDivisionError for dividing by zero' do
+      expect{
+        load './lib/a_division_by_zero_error.rb'
+      }.to_not raise_error
+    end
